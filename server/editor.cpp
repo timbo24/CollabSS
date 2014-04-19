966fff713a1536de86daeddbb937da0793097512
tkkemo@gmail.com <tkkemo@gmail.com>
2014-04-19 01:24:42 -0600

    yay, file list outputs and displays in clientls

--- server/editor.cpp
+++ server/editor.cpp
@@ -150,7 +150,7 @@ void spreadsheet_editor::incoming_message(std::string message)
 	trim(message);
 
 	size_t pos = 0;
-	std::string delimiter = "\e";
+	std::string delimiter = "\\e";
 
 	pos = message.find(delimiter);
 	std::string token = message.substr(0, pos);
@@ -164,7 +164,7 @@ void spreadsheet_editor::incoming_message(std::string message)
 	{
 		if (message == PASSWORD)
 		{
-			outm = "FILELIST\e";
+			outm = "FILELIST\\e";
 
 			std::set<spreadsheet_session_ptr> sessions = server_->get_spreadsheets();
 
@@ -172,11 +172,11 @@ void spreadsheet_editor::incoming_message(std::string message)
 			
 			for (auto i = sessions.begin(); i != sessions.end(); ++i)
 			{
-				outm += (*i)->get_name() + "\e";
+				outm += (*i)->get_name() + "\\e";
 				std::cout<<(*i)->get_name()<<std::endl;
 			}
 
-			outm += "\r\n"
+			outm += "\r\n";
 			
 			std::cout<<"outgoing: " << outm << std::endl;
 			deliver(outm);
