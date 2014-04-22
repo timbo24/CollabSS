d335c216b90c2f16eaaa251d23d123e1b7bcbe6c
tkkemo@gmail.com <tkkemo@gmail.com>
2014-04-22 03:26:57 -0600

    someshit

--- 3505_SS/SpreadsheetGUI/Form1.cs
+++ 3505_SS/SpreadsheetGUI/Form1.cs
@@ -385,6 +385,17 @@ namespace SS
                         MessageBoxButtons.YesNo, MessageBoxIcon.Warning))
                 {
                     case DialogResult.Yes:
+                        Thread oThread = new Thread(new ThreadStart(() =>
+                        {
+                            Application.EnableVisualStyles();
+                            Application.SetCompatibleTextRenderingDefault(false);
+                            // Start an application context and run one form inside it
+                            DemoApplicationContext appContext = DemoApplicationContext.getAppContext();
+                            appContext.RunForm(new FormLogin());
+                            Application.Run(appContext);
+
+                        }));
+                        oThread.Start();
                         return;
                     case DialogResult.No:
                         model.CloseLogin();
